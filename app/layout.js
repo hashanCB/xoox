1fc3505da70d14ddc94993d42a5c431ac252c0be
chanaka <hashan.chanaka96@gmail.com>
2025-02-20 10:45:12 +0000

    prettier add

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import Header from "@/app/components/Header";
 
 export const metadata = {
   title: "XooX | Next.js E-Commerce",
@@ -19,9 +10,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body>
+        <Header />
         {children}
       </body>
     </html>
